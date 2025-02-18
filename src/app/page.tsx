e3ca4ea3318576201c633a81edfbd71a92f2c23b
Joseph Williams <jw69455@gmail.com>
2025-02-18 17:43:35 -0600

    menu updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,66 @@
-'use client';
+"use client";
 
 import { useState } from "react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import { CircleX, Eye, EyeOff } from "lucide-react";
+import { Eye, EyeOff } from "lucide-react";
 
-export default function Login() {
+export default function Passcode() {
 
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
-  const router = useRouter();
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const router = useRouter();
+    const [showPassword, setShowPassword] = useState(false);
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setIsLoading(true);
-    setError('');
+    const handlePasswordLogin = () => {
+        if (password === process.env.NEXT_PUBLIC_ACCESS_PASSWORD) {
+            window.location.href = 'https://qrco.de/holidaymenu';
+        } else {
+            setError("Incorrect password");
+        }
+    };
 
-    try {
-      const response = await fetch('/api/getPassword', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ password })
-      });
-      
-      const data = await response.json();
-      
-      if (data.success) {
-        localStorage.setItem('isAuthenticated', 'true');
-        router.push('/houston');
-      } else {
-        setError('Incorrect password');
-      }
-    } catch (err) {
-      setError('An error occurred');
-    }
-    
-    setIsLoading(false);
-  };
-
-  return (
-    <div className="bg-[#57c18e]">
-      <form onSubmit={handleSubmit}>
-        <div className="flex h-screen w-full flex-col items-center justify-center px-4 py-12">
-          <div className="mb-10 animate-wiggle">
-            <Image src="/tinytreelogo.png" width={100} height={100} alt="Welcome Logo"  />
-          </div>
-          <div>
-            <h1 className="text-white text-4xl mb-8 font-semibold">Welcome to Tiny Trees</h1>
-          </div>
-          <div className="grid w-2/5 text-black items-center mb-6 relative">
-            <Input 
-              type={showPassword ? "text" : "password"}
-              id="password" 
-              placeholder="*******" 
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)} 
-            />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
-            >
-              {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-            </button>
-          </div>
-          {error && (<div className="flex flex-row text-red-500 font-thin text-sm"><CircleX /> {error}</div>)}
-          <div>
-            <Button className="bg-[#007a58] hover:bg-[#007a58]/75 font-semibold w-60 h-11 mt-6" type="submit" disabled={isLoading}>
-              {isLoading ? 'Loading...' : 'Enter'}
-            </Button>
-          </div>
+    return (
+        <div className="min-h-screen bg-[#57c18e] flex items-center justify-center p-4">
+            <div className="flex flex-col items-center text-center">
+                <div className="flex flex-col items-center mb-8">
+                    <Image src="/new_TT_logo.png" width={115} height={115} alt="Welcome Logo" />
+                </div>
+                <div className="flex flex-col items-start w-full max-w-[288px]">
+                    <Label htmlFor="password" className="text-white font-semibold text-md">Password</Label>
+                    <div className="relative w-full">
+                    <Input 
+                        type={showPassword ? "text" : "password"} 
+                        placeholder="********" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="max-w-[288px]"
+                    />
+                    <button
+                        type="button"
+                        onClick={() => setShowPassword(!showPassword)}
+                        className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                    >
+                        {showPassword ? (
+                            <EyeOff size={20} />
+                        ) : (
+                            <Eye size={20} />
+                        )}
+                    </button>
+                    </div>
+                    {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+                    <Button 
+                        size="lg" 
+                        className="bg-[#007a58] hover:bg-[#007a58]/75 w-72 h-11 mt-4" 
+                        onClick={handlePasswordLogin}
+                    >
+                        Enter
+                    </Button>
+                </div>
+            </div>
         </div>
-      </form>
-    </div>
-  );
-};
+    );
+}
