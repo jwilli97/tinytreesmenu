bcf3bcd857f2e0351d55bcf981986415e65f938b
Joseph Williams <jw69455@gmail.com>
2025-02-21 19:01:08 -0600

    security updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,8 @@ export default function LoginPage() {
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     
-    if (accessCode === process.env.NEXT_PUBLIC_HOUSTON_PASSWORD) {
+    if (accessCode === process.env.NEXT_PUBLIC_HOUSTON_PASSWORD || 
+        accessCode === process.env.NEXT_PUBLIC_HOUSTON2_PASSWORD) {
       // Redirect to holiday menu QR code
       window.location.href = "https://qrco.de/holidaymenu";
     } else if (accessCode === process.env.NEXT_PUBLIC_AUSTIN_PASSWORD) {
